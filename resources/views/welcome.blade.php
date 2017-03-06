1ce8b3f974deee6c89cd3a9f82b4c3f0405d6789
Alban Mulaki <alban.mulaki@gmail.com>
2017-03-06 23:54:49 +0100

    Language Package configurated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,18 @@
+@include('js-localization::head')
 <!DOCTYPE html>
+
 <html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>@Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        @yield('js-localization.head')
+        <script src="/js/app.js"></script>
         <!-- Styles -->
         <style>
             html, body {
@@ -66,15 +69,16 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+            @lang('auth.failed')
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
+            <div class="top-right links">
+                @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
+                @endif
+            </div>
             @endif
 
             <div class="content">
