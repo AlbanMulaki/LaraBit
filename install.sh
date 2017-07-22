a75f4921837d45b4f584eaa07a704484fb8102ee
Alban Mulaki <alban.mulaki@gmail.com>
2017-07-22 10:23:16 +0200

    Install Improvment

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 cp ./larabit/.env.example ./larabit/.env
-docker-compose build --no-cache
-docker-compose up --force-recreate
+docker-compose build
+#docker-compose build --no-cache
+docker-compose up --force-recreate -d
+docker-compose exec web sh /build.sh
+docker-compose exec web cd /var/www && npm install -g && npm install gulp -g && gulp 
+--development 
