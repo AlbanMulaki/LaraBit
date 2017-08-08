feb586f6d62a8422daf04368da42120126ad08b9
Alban Mulaki <alban.mulaki@gmail.com>
2017-08-08 10:00:25 +0200

    Remove unused views from system module

--- larabit/app/Core/System/Views/includes/stylesheets.blade.php
+++
@@ -1,4 +0,0 @@
-<link href="{{ asset('/css/lib/bootstrap.min.css')}}" rel="stylesheet">
-<link href="{{ asset('/css/lib/font-awesome.min.css')}}" rel="stylesheet">
-<link href="{{ asset('/css/lib/icheck.min.css')}}" rel="stylesheet">
-<link href="{{ asset('/css/lib/custom.min.css')}}" rel="stylesheet">
--- larabit/app/Core/System/Views/roles/partials/tree-modules.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-	<td class="a-center " colspan='100'>
-		<i class="permission-tree-level-1 fa fa-lg fa-cube"> </i> {{ ucfirst($key) }}
-	</td>
-</tr>
-
-@each('users::roles.partials.tree-permission',$module['permissions'],'permission')
